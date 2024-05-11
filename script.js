1f075437789f073409a153124decf409b2c4a509
skt avinash <118832769+sktavinash@users.noreply.github.com>
2024-05-12 00:29:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function rollDice(){
+    const numberOfDice = document.getElementById("numberOfDice").value
+    const diceResult = document.getElementById("diceResult")
+    const diceImages = document.getElementById("diceImages")
+    const values =[];
+    const images = [];
+
+
+    for(let i = 0;i < numberOfDice; i++ ){
+        const value = Math.floor(Math.random()*6)+1;
+        values.push(value) 
+        images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`)       
+    }
+
+  diceResult.textContent=  `dice:${values.join(', ')}`
+  diceImages.innerHTML = images.join(``);
+}
