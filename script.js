42fc19e2342427b75f841b4d29aa40a1c565d97c
skt avinash <118832769+sktavinash@users.noreply.github.com>
2024-05-12 00:50:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function rollDice(){
     for(let i = 0;i < numberOfDice; i++ ){
         const value = Math.floor(Math.random()*6)+1;
         values.push(value) 
-        images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`)       
+        images.push(`<img src="${value}.png" alt="Dice ${value}">`)       
     }
 
   diceResult.textContent=  `dice:${values.join(', ')}`
